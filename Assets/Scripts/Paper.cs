beb7f8515deb22fed18315e919cbe6174c2d4db3
Dan Bially Levy <dbiallyl@macalester.edu>
2023-02-19 16:51:27 -0600

    Moved in test movement code
    Accidentally created a new scene for this - we could probably get rid of SampleScene?

---
+++ Assets/Scripts/Paper.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper : MonoBehaviour
+{
+    public GameObject selectedObject;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePage();
+    }
+
+    void MovePage() {
+        // Code from: https://gamedevbeginner.com/how-to-move-an-object-with-the-mouse-in-unity-in-2d/
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject;
+                offset = selectedObject.transform.position - mousePosition;
+            }
+        }
+        if (selectedObject)
+        {
+            selectedObject.transform.position = mousePosition + offset;
+        }
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+            selectedObject = null;
+        }
+    }
+}
