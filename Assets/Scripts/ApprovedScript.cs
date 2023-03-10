b6be947b294f83369286f69774dd8fff50d554cf
echen <98108241+echen642@users.noreply.github.com>
2023-03-10 13:10:00 -0600

    Got the Stamper to stamp when a certain area of the paper is underneath the stamper

--- Assets/Scripts/ApprovedScript.cs
+++ Assets/Scripts/ApprovedScript.cs
@@ -6,6 +6,9 @@ public class ApprovedScript : MonoBehaviour
 {
     public SpriteRenderer myRenderer;
     public Sprite stampType;
+    public float stampScale;
+    public float stampX;
+    public float stampY;
     // public BoxCollider2D collider;
     // Start is called before the first frame update
     void Start()
@@ -23,20 +26,22 @@ public class ApprovedScript : MonoBehaviour
     [ContextMenu("Pressed on me")]
     public void OnMouseDown()
     {
-        BoxCollider2D col = gameObject.GetComponent<BoxCollider2D>();
-        Bounds bound = col.bounds;
+        BoxCollider2D stampCollider = gameObject.GetComponent<BoxCollider2D>();
+        Bounds stampBounds = stampCollider.bounds;
         // Debug.Log(bound.extents);
-        Debug.Log(gameObject.transform.position);
-        float width1 = gameObject.transform.position.x - bound.extents.x;   // width1 and width2 will give me 
-        float width2 = gameObject.transform.position.x + bound.extents.x;   // the x-range which the stamp exists at
+        // Debug.Log(gameObject.transform.position);
+        //float widthLeft = gameObject.transform.position.x - bound.extents.x;   // width1 and width2 will give me the
+        //float widthRight = gameObject.transform.position.x + bound.extents.x;   // the x-range which the stamp exists at
 
-        float height1 = gameObject.transform.position.y - bound.extents.y;  // height1 and height 2 will give me the
-        float height2 = gameObject.transform.position.y + bound.extents.y;  // y-range which the stamp exists at
+        //float heightBottom = gameObject.transform.position.y - bound.extents.y;  // height1 and height 2 will give me the
+        //float heightTop = gameObject.transform.position.y + bound.extents.y;  // y-range which the stamp exists at
 
         // If the paper is within the ranges defined above, I will set the stamp picture to be a child of the paper.
 
         GameObject[] papers = GameObject.FindGameObjectsWithTag("Pages");
-        int highestPage = 6;
+        Debug.Log("This is stampBounds.max " + stampBounds.max);
+        Debug.Log("This is stampBounds.min " + stampBounds.min);
+        // int highestPage = 6;
         foreach (GameObject paper in papers)
         {
             // Check if page is the top page in the Pages Sorting Layer
@@ -46,21 +51,29 @@ public class ApprovedScript : MonoBehaviour
             // Check if the paper is within the ranges
             BoxCollider2D paperCollider = paper.GetComponent<BoxCollider2D>();
             Bounds paperBounds = paperCollider.bounds;
-            float paperLeftX = paper.transform.position.x - paperBounds.extents.x;
-            float paperRightX = paper.transform.position.x + paperBounds.extents.x;
 
-            float paperTop = paper.transform.position.y + paperBounds.extents.y;
-            float paperBottom = paper.transform.position.y - paperBounds.extents.y;
+            // var means figure out the data type for me
+            var intersection = 
+                Vector3.Min(stampBounds.max, paperBounds.max)
+                - Vector3.Max(stampBounds.min, paperBounds.min);
 
+
+            if (intersection.x > 0 && intersection.y > 0)       // Both intersection.x and intersection.y should be positive because negative width and height shouldn't result in an area
+            {
+                if (stampBounds.Intersects(paperBounds))
+                {
+                    Debug.Log("Paper is intersecting with the stamper!");
+                    float area = intersection.x * intersection.y;
+                    if (area > 3)
+                    {
+                        Debug.Log("This is the paper: " + paper + "and this is my area: " + area);
+                        ApplyStamp(paper);
+                    }
+                }
+
+                
+            }
         }
-
-        Debug.Log("Left: " + width1);
-        Debug.Log("Right: " + width2);
-
-        Debug.Log("Top: " + height2);
-        Debug.Log("Bottom: " + height1);
-
-
     }
 
     private GameObject checkTopPaper()
@@ -68,11 +81,19 @@ public class ApprovedScript : MonoBehaviour
         return null;
     }
 
-    private void ApplyStamp()
+    private void ApplyStamp(GameObject paper)
     {
         Debug.Log("Stamped Approved!");
         GameObject approvedStamp = new GameObject("Approved Stamp Picture");
+        Vector3 stampPosition = approvedStamp.transform.position;
+        approvedStamp.transform.SetParent(paper.transform);
         SpriteRenderer stampRenderer = approvedStamp.AddComponent<SpriteRenderer>();
         stampRenderer.sprite = stampType;
+        approvedStamp.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+        Debug.Log("This is the current position: " + approvedStamp.transform.position);
+        stampPosition.x = -9;
+        stampPosition.y = 3;
+        
+
     }
 }
