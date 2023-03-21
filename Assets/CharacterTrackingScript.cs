353ea149563e7897cf19545ffc2e4b75d8bbbfb0
echen <98108241+echen642@users.noreply.github.com>
2023-03-21 10:41:02 -0500

    Adding a Character Tracking Script

---
+++ Assets/CharacterTrackingScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTrackingScript : MonoBehaviour
+{
+    // Categories to think about:
+    // Neither oldest or youngest
+    // Oldest
+    // Youngest
+    // Highest income
+    // Lowest income
+    // Neither highest or lowest income
+    // Not well-known
+    // Well-known (extremely)
+    // Well-known (moderately)
+    // No good
+    // Potential good
+    // Guaranteed good
+    // Longest on waitlist
+    // Shortest on waitlist
+
+    int oldest = 0;
+    int youngest = 0;
+    int highestIncome = 0;
+    int lowestIncome = 0;
+    int neitherIncome = 0;
+    int notKnown = 0;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
