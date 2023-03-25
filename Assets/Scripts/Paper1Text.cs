1503ea3096b4411f108657ab7b89f73e6639745b
Ana Espeleta <89929081+anaespeleta115@users.noreply.github.com>
2023-03-25 14:26:04 -0500

    created text script

---
+++ Assets/Scripts/Paper1Text.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper1Text : MonoBehaviour
+{
+    public GameObject textmeshpro_name;
+    public GameObject textmeshpro_age;
+    public GameObject textmeshpro_occupation;
+    public GameObject textmeshpro_time;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
