23b33ff3af7e9f3b68c421b022450b057a66cd7b
Dan Bially Levy <dbiallyl@macalester.edu>
2023-03-29 14:11:34 -0500

    Working on combining Scene Managers

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,6 +6,12 @@ public class SoundManager : MonoBehaviour
 {
     public AudioSource[] audioSources;
     int timer;
+
+    public GameObject selectedObject;
+    public GameObject[] pagesArray;
+    public GameObject lastlookedat;
+    int currentMax;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +21,78 @@ public class SoundManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ManageSound();
+        ManagePages();
+    }
 
+    void ManageSound() {
         if((timer >= 1)&&(timer<=2000)){timer--;}
         else if(timer < 1){
             print("go");
             audioSources[1].Play();
             timer = 3000;
         }
+    }
+
+
+    // Update is called once per frame
+    void ManagePages()
+    {
+     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                if(targetObject.transform.gameObject.tag == "Pages"){
+                    selectedObject = targetObject.transform.gameObject;
+                    print(selectedObject.name);
+                    if(selectedObject.name != pagesArray[3].name){
+                        pagesArray[4] = pagesArray[3];
+                        pagesArray[3] = null;
+                        for(int i=0; i<3;i++){
+                            if(selectedObject.name == pagesArray[i].name){
+                                currentMax = i;
+                                break;
+                            }
+                        }
+                        pagesArray[3] = pagesArray[currentMax];
+                        pagesArray[3].SendMessage("LayerUpdate", 6);
+                        for(int i=currentMax+1; i<3;i++){
+                            pagesArray[i-1]=pagesArray[i];
+                        }
+                        
+                        pagesArray[2]=pagesArray[4];
+                        pagesArray[4]=null;
+                        pagesArray[2].SendMessage("LayerUpdate",4);
+                        for(int i=0; i<3;i++){
+                            pagesArray[i].SendMessage("LayerUpdate", 2*i);
+                        }
+                    }
+                    else
+                    {
+                        pagesArray[3].SendMessage("LayerUpdate", 6);
+                    }
+                }
+            }   
+        } else if(Input.GetMouseButtonDown(1)){
+        Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+        if (targetObject)
+            {
+            if((targetObject.transform.gameObject.tag == "Pages") || (targetObject.transform.gameObject.tag == "Phone")){
+                selectedObject = targetObject.transform.gameObject;
+                print(selectedObject.name);
+                if(selectedObject != lastlookedat){
+                    selectedObject.SendMessage("StartLookingAt");
+                    lastlookedat.SendMessage("StopLookingAt");
+                    lastlookedat=selectedObject;
+                } else {
+                    selectedObject.SendMessage("StopLookingAt");
+                    lastlookedat=gameObject;
+                }
+            }
+
+            }
+    }
     }
 }
