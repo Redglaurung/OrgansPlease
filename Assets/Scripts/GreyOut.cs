91d1cce18b6f5181752de81b9ccb9cb3e6ecc062
Redglaurung <43708381+Redglaurung@users.noreply.github.com>
2023-04-17 13:20:23 -0500

    Update GreyOut.cs

--- Assets/Scripts/GreyOut.cs
+++ Assets/Scripts/GreyOut.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GreyOut : MonoBehaviour
 {
+
+    SpriteRenderer myRenderer;
+    float opacity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        opacity = 1f;
+        myRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        myRenderer.color = new Color(1f,1f,1f,1f);
         
     }
 }
