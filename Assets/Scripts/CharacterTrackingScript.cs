8d90605a94f0bc1c9616174cbdb9017e32684bae
Dan Bially Levy <dbiallyl@macalester.edu>
2023-04-23 11:01:10 -0500

    Finished adding Day 5
    Also fixed some things across scene (no sound, no boss name) and added transitions

--- Assets/Scripts/CharacterTrackingScript.cs
+++ Assets/Scripts/CharacterTrackingScript.cs
@@ -8,6 +8,7 @@ public class CharacterTrackingScript : MonoBehaviour
     SummaryTrackingScript summary;
     private bool day1Chosen = false;
     private bool day2Chosen = false;
+    private bool day5Chosen = false;
     private bool day6Chosen = false;
 
     // Start is called before the first frame update
@@ -129,6 +130,56 @@ public class CharacterTrackingScript : MonoBehaviour
             }
         }
 
+        if (SceneManager.GetActiveScene().name == "Day5")
+        {
+            if (day5Chosen == false)
+            {
+                foreach (GameObject paper in papers)
+                {
+                    Pages paperScript = paper.GetComponent<Pages>();
+                    if (paperScript.isStamped)
+                    {
+                        day5Chosen = true;
+                        if (paper.name == "Paper1") // Nawel Berkowitz
+                        {
+                            summary.youngest += 1;
+                            summary.lowestIncome += 1;
+                            summary.midKnown += 1;
+                            summary.noGood += 1;
+                            summary.neitherWait += 1;
+                        }
+
+                        if (paper.name == "Paper2")     // Hana Yoon
+                        {
+                            summary.neitherAge += 1;
+                            summary.neitherIncome += 1;
+                            summary.midKnown += 1;
+                            summary.guarGood += 1;
+                            summary.shortWait += 1;
+                        }
+
+                        if (paper.name == "Paper3") // Ariel Cilenti
+                        {
+                            summary.oldest += 1;
+                            summary.highestIncome += 1;
+                            summary.wellKnown += 1;
+                            summary.noGood += 1;
+                            summary.neitherWait += 1;
+                        }
+
+                        if (paper.name == "Paper4") // Theo Gerhardt
+                        {
+                            summary.neitherAge += 1;
+                            summary.neitherIncome += 1;
+                            summary.notKnown += 1;
+                            summary.guarGood += 1;
+                            summary.longWait += 1;
+                        }
+                    }
+                }
+            }
+        }
+
         if (SceneManager.GetActiveScene().name == "Day6")
         {
             if (day6Chosen == false)
@@ -190,6 +241,10 @@ public class CharacterTrackingScript : MonoBehaviour
         if (SceneManager.GetActiveScene().name == "Day 2")
         {
             return day2Chosen;
+        }
+        if (SceneManager.GetActiveScene().name == "Day5")
+        {
+            return day5Chosen;
         }
         if (SceneManager.GetActiveScene().name == "Day6")
         {
